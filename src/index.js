86927b22715d87adcb9b2d03b50e760f3af34f36
alangohe845 <118926690+alangohe845@users.noreply.github.com>
2023-01-13 14:37:47 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi practica con CodeSandbox</h1>
+<div>
+  <h2>ALAN NAHUM GOMEZ HERNANDEZ</h2>
+  Universidad Tecnologica de Durango 
+  <a href="https://github.com/alangohe845/Contador.git" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
